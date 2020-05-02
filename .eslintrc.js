b67fb026b4a467165ea1b9a517731301d793428b
Masoud Pezeshkzade <masoud.pezeshkzade@gmail.com>
2020-05-03 04:04:10 +0430

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser:  "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/prop-types':'off',
+    'react/display-name':'off',
+  }
+}

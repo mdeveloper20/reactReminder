019da70a899c1bfb5059cc6c9a381d166b372ed8
Masoud Pezeshkzade <masoud.pezeshkzade@gmail.com>
2020-08-01 20:42:49 +0430

    project implemented.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classNames from 'classnames'
 import './App.css'
-import Register from './Register/Register'
 
 function App () {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onClickHeader = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <div className='app'>
-      <Register/>
-      <h3>React Select <b>Creatable</b> Example</h3>
+      <h3>Conditional styling using <b>ClassNames</b> package in ReactJS</h3>
 
+      <div className='box'>
+        <p onClick={onClickHeader} className="title">Click me! <i className="arrow-up"></i></p>
+
+        <div className={classNames('description', { 'description-active': isOpen, 'green-active': isOpen })}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+      </div>
     </div>
   )
 }

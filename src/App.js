2a5cbe57c2c69d285d897194ed732fcaad134d40
Masoud Pezeshkzade <masoud.pezeshkzade@gmail.com>
2020-08-25 00:15:11 +0430

    color picker added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React, { useState } from 'react'
 import './App.css'
 
+import { BlockPicker } from 'react-color'
 import Tippy from '@tippyjs/react'
-import 'tippy.js/dist/tippy.css' // optional
-import 'tippy.js/themes/light.css'
-import 'tippy.js/animations/perspective.css'
 
 function App () {
+  const [selectedColor, setSelectedColor] = useState('#ccc')
   return (
     <div className='app'>
-      <h1>Tippy JS React Tutorial</h1>
+      <h1>Tippy JS + React Color  Tutorial</h1>
 
-      <div className='area'>
-        <Tippy placement={'bottom'} animation='perspective' theme={'light'} interactive={true} content={
-          <Tippy offset={[0, 20]} placement={'bottom'} animation='perspective' theme={'light'} interactive={true} content={<div>
-            <h1>Hello this is my first tooltip using Tippy</h1>
-          </div>}>
-            <button className='ref-button2'>
-            Click on me!
-            </button>
-          </Tippy>
-        }>
-          <button className='ref-button'>
-          Click on me!
-          </button>
-        </Tippy>
+      <div className='area' style={{ backgroundColor: selectedColor }}>
+
       </div>
+
+      <Tippy interactive={true} placement={'bottom'} content={
+        <BlockPicker
+          color={selectedColor}
+          onChangeComplete={color => setSelectedColor(color.hex)}
+        />
+      }>
+        <button className='ref-button'>Change box color</button>
+
+      </Tippy>
 
     </div>
   )

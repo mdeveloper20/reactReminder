b030f765c1cb16a00eb9198bc8ab811bccd47464
Masoud Pezeshkzade <masoud.pezeshkzade@gmail.com>
2020-08-27 21:00:20 +0430

    animation implemented.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { useState } from 'react'
 import './App.css'
-
-import { BlockPicker } from 'react-color'
-import Tippy from '@tippyjs/react'
+import AnimatedNumber from 'react-animated-number'
 
 function App () {
-  const [selectedColor, setSelectedColor] = useState('#ccc')
+  const [counter, setCounter] = useState(0)
+
   return (
     <div className='app'>
-      <h1>Tippy JS + React Color  Tutorial</h1>
+      <h1>React Animated Number Tutorial</h1>
 
-      <div className='area' style={{ backgroundColor: selectedColor }}>
+      <AnimatedNumber
+        value={counter}
+        style={
+          {
+            fontSize: 200
+          }
+        }
+        formatValue={n => n.toFixed(0)}
+        frameStyle={percentage => percentage > 20 && percentage < 80 ? { opacity: 0.5 } : {}}
+      />
+      <div className='likes'>Likes for this video</div>
 
+      <div className='area'>
+        <button onClick={() => setCounter(counter - 50)} className='button' >Decrease</button>
+        <button onClick={() => setCounter(counter + 50)} className='button green'>Increase</button>
       </div>
-
-      <Tippy interactive={true} placement={'bottom'} content={
-        <BlockPicker
-          color={selectedColor}
-          onChangeComplete={color => setSelectedColor(color.hex)}
-        />
-      }>
-        <button className='ref-button'>Change box color</button>
-
-      </Tippy>
 
     </div>
   )

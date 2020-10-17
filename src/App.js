56b23ae056bed2e17a1c4339cdf200b18d66d222
Masoud Pezeshkzade <masoud.creatella@gmail.com>
2020-10-17 16:47:17 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import './App.css'
-import VideoChatContainer from './VideoChatContainer'
 
 function App () {
   return (
     <div className='app'>
-      <h1>React Video Chat App</h1>
-      <h2>WebRTC + Firebase</h2>
-      <VideoChatContainer/>
+      test
     </div>
   )
 }
